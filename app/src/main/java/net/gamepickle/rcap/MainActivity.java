9221c1ab46561b9bb413f3d988f1a867763796d0
ClaymoreAdrendamar <adrendamar@gmail.com>
2016-12-20 19:32:48 +0100

    Creation of app

---
+++ app/src/main/java/net/gamepickle/rcap/MainActivity.java
@@ -0,0 +1,22 @@
+package net.gamepickle.rcap;
+
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
