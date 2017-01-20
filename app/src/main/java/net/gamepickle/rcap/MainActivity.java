bd4807b8b5110e92ee2c28d145db32519c3674ea
ClaymoreAdrendamar <adrendamar@gmail.com>
2017-01-20 19:14:44 +0100

    New changelist

--- app/src/main/java/net/gamepickle/rcap/MainActivity.java
+++ app/src/main/java/net/gamepickle/rcap/MainActivity.java
@@ -1,16 +1,18 @@
 package net.gamepickle.rcap;
 
-import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    GameVariables gameVariables;
+    GameVariables gameVars;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        gameVariables = new GameVariables(this);
+        gameVars = new GameVariables(this);
+        TextView t = (TextView) findViewById(R.id.cash_text);
+        t.setText(String.valueOf(gameVars.getInt("CASH")));
     }
 
 }
